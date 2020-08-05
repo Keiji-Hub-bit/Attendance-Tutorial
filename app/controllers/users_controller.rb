adf4d45953d21b958ec444567c581e8f0e98166c
Keiji Tadokoro <gccx.kt.0925@icloud.com>
2020-08-05 23:46:09 +0000

    5章5.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
    def show
     @user = User.find(params[:id])
-  end
+   end
   
-  def new
+   def new
+     @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
 end
