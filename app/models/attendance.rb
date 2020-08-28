56cc02a5a637edc8460cfc024180a18e49091f47
Keiji Tadokoro <gccx.kt.0925@icloud.com>
2020-08-28 14:35:42 +0000

    10章10.1.3完了

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,7 +8,7 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   
   def finished_at_is_invalid_without_a_started_at
-    errors.add(:srarted_at, "が必要です")if started_at.blank? && finished_at.present?
+    errors.add(:started_at, "が必要です")if started_at.blank? && finished_at.present?
   end
 end
 
